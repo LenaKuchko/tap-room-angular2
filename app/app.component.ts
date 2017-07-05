ccc2bae51a28b2d4305a7ff387fa684712e55f62
Olena Kuchko <kuchkoel@gmail.com>
2017-07-05 15:51:23 -0700

    fix issue with show new keg form

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,16 +7,18 @@ import {Keg} from './keg.model';
   <div class="container">
     <h1>Tap Room</h1>
     <h3>All available kegs</h3>
+    <button (click)='startAdding()' >Add</button>
     <keg-list [childKegList]="masterKegList" (clickSender)='editKeg($event)'></keg-list>
     <hr>
     <edit-keg [childSelectedKeg]='selectedKeg' (doneButtonClickedSender)='finishedEditing()'></edit-keg>
-    <new-keg (newKegSender)='addKeg($event)'></new-keg>
+    <new-keg [startAdd]='start' (newKegSender)='addKeg($event)'></new-keg>
   </div>
   `
 })
 
 export class AppComponent {
   selectedKeg = null;
+  start: boolean = false;
 
   masterKegList: Keg[] = [
     new Keg("Keg1", "Belgium", 45, 3, 5)
@@ -33,5 +35,10 @@ export class AppComponent {
   editKeg(clickedKeg: Keg)
   {
     this.selectedKeg = clickedKeg;
+  }
+
+  startAdding()
+  {
+    this.start = true;
   }
 }
--- app/new-keg.component.ts
+++ app/new-keg.component.ts
@@ -1,42 +1,46 @@
-import {Component, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {Keg} from './keg.model';
 
 @Component({
   selector: 'new-keg',
   template:`
-  <h1>New Keg</h1>
-  <div>
-    <label>Enter Keg Name:</label>
-    <input #newName>
-  </div>
-  <div>
-    <label>Enter Keg Brand Name:</label>
-    <input #newBrandName>
-  </div>
-  <div>
-    <label>Enter Price:</label>
-    <input #newPrice>
-  </div>
-  <div>
-    <label>Enter alcoholContent:</label>
-    <input #newAlcoholContent>
-  </div>
-  <div>
-    <label>Enter Volume:</label>
-    <select #newVolume>
-      <option [value]="5"> 5 gal </option>
-      <option [value]="7"> 7 gal </option>
-      <option [value]="10"> 10 gal </option>
-      <option [value]="15"> 15 gal </option>
-    </select>
-    <button class='btn' (click)='submitForm(newName.value, newBrandName.value, newPrice.value, newAlcoholContent.value, newVolume.value)'>Add new Keg</button>
+
+  <div *ngIf='startAdd'>
+    <h1>New Keg</h1>
+    <div>
+      <label>Enter Keg Name:</label>
+      <input #newName>
+    </div>
+    <div>
+      <label>Enter Keg Brand Name:</label>
+      <input #newBrandName>
+    </div>
+    <div>
+      <label>Enter Price:</label>
+      <input #newPrice>
+    </div>
+    <div>
+      <label>Enter alcoholContent:</label>
+      <input #newAlcoholContent>
+    </div>
+    <div>
+      <label>Enter Volume:</label>
+      <select #newVolume>
+        <option [value]="5"> 5 gal </option>
+        <option [value]="7"> 7 gal </option>
+        <option [value]="10"> 10 gal </option>
+        <option [value]="15"> 15 gal </option>
+      </select>
+      <button class='btn' (click)='submitForm(newName.value, newBrandName.value, newPrice.value, newAlcoholContent.value, newVolume.value)'>Add new Keg</button>
+    </div>
   </div>
   `
 })
 
 export class NewKegComponent {
   @Output() newKegSender = new EventEmitter();
-
+  @Output() addButtonClickedSender = new EventEmitter();
+  @Input() startAdd: boolean;
   submitForm(name: string, brand: string, price: number, alcoholContent: number, volume: number) {
     const newKegToAdd: Keg = new Keg(name, brand, price, alcoholContent, volume);
     this.newKegSender.emit(newKegToAdd);
